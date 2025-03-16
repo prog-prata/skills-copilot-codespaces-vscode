8f9ef776c776f5bc8233a636aedadfb61d53f835
prog-prata <programador.prateado@gmail.com>
2025-03-16 22:02:49 +0000

    Copilot third commit again

--- comments.js
+++ comments.js
@@ -1,30 +1,15 @@
-// Create a web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-// Create a web server
-http.createServer(function (req, res) {
-  // Parse the request containing file name
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  console.log(filename);
-  // Check if file exists
-  fs.exists(filename, function (exists) {
-    if (exists) {
-      // If file exists read it and return it
-      fs.readFile(filename, function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-      });
-    } else {
-      // If file does not exist return 404
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.write("404 Not Found");
-      return res.end();
-    }
-  });
-}).listen(8080);
-// Console will print the message
-console.log('Server running at http://  localhost:8080/');
+// Create web server
+// 1. Import express
+const express = require('express');
+// 2. Create express server
+const app = express();
+// 3. Create port
+const port = 8080;
+// 4. Create response
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+// 5. Listen port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
