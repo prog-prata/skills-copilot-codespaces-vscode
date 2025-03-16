50454a54c6020d69fce48586055522fc673617e7
prog-prata <programador.prateado@gmail.com>
2025-03-16 21:45:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <div className="skills-member__header">
+        <h2 className="skills-member__title">Skills</h2>
+      </div>
+      <div className="skills-member__body">
+        <ul className="skills-member__list">
+          <li className="skills-member__item">HTML</li>
+          <li className="skills-member__item">CSS</li>
+          <li className="skills-member__item">JavaScript</li>
+          <li className="skills-member__item">React</li>
+          <li className="skills-member__item">Node.js</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
